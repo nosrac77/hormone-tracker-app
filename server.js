64f09d58cbf4eae4ee635872a57ac7e3296a0901
nosrac77 <carson.newton@outlook.com>
2017-09-09 12:57:58 -0700

    took out ./public to identify issue

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const client = new pg.Client(conString);
 pg.defaults.ssl = true;
 client.connect();
 client.on('error', err => console.error('you fucked up somehow man, here\'s the error ' + err));
-app.use(express.static('./public'));
+app.use(express.static('./'));
 app.listen(PORT, function() {
   console.log('Listening on port ' + PORT);
 });
