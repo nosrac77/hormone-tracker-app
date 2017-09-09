28bad39e95c543cca89820ec526adf6eff24253e
Kyle Richey <richey.kyle.i@gmail.com>
2017-09-09 11:17:15 -0700

    forgot to switch branches

--- public/scripts/view.js
+++ public/scripts/view.js
@@ -3,4 +3,16 @@
 Datapoint.handleTabs = function(event){
   $('.data').hide();
   //$('.tab').on('click', event => { hide id's that match the clicked tabs' data category }
-}
+};
+
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart (ctx, {
+  type: 'line',
+  data: {
+    labels: [dataPoints.date],
+    datasets: [{
+      label: "Estrogen",
+      
+    }]
+  },
+});
