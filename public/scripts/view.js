4f7bbb88d9c28008cc0058651abf4387e4b8fa5d
Kyle Richey <richey.kyle.i@gmail.com>
2017-09-09 11:55:44 -0700

    finished graph just needs data

--- public/scripts/view.js
+++ public/scripts/view.js
@@ -1,6 +1,6 @@
 'use strict';
 
-Datapoint.handleTabs = function(event){
+DataPoint.handleTabs = function(event){
   $('.data').hide();
   //$('.tab').on('click', event => { hide id's that match the clicked tabs' data category }
 };
@@ -12,7 +12,29 @@ var chart = new Chart (ctx, {
     labels: [dataPoints.date],
     datasets: [{
       label: "Estrogen",
-      
+      backgroundColor: 'rgb(0,0,0)',
+      borderColor: 'rgb(238, 117, 234)',
+      borderWidth: '3px',
+      pointBackgroundColor: 'rgb(224, 224, 224)',
+      pointBorderColor: 'rgb(238, 117, 234)',
+      pointBorderWidth: '1px',
+      pointStyle: 'circle',
+      showLines: true,
+      fontColor: 'rgb(255, 255, 255)',
+      data: [dataPoints.eLevel],
+    },
+    {
+      label: "Testosterone",
+      backgroundColor: 'rgb(0,0,0)',
+      borderColor: 'rgb(0, 255, 255)',
+      borderWidth: '3px',
+      pointBackgroundColor: 'rgb(224, 224, 224)',
+      pointBorderColor: 'rgb(0, 255, 255)',
+      pointBorderWidth: '1px',
+      pointStyle: 'circle',
+      showLines: true,
+      fontColor: 'rgb(255, 255, 255)',
+      data: [dataPoints.tLevel],
     }]
   },
 });
