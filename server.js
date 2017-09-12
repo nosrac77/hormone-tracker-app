b8beac46a2331cb62e0c147d5beeba5cdaef7eff
nosrac77 <carson.newton@outlook.com>
2017-09-11 19:56:54 -0700

    commented out pg defaults line in server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const conString = process.env.DATABASE_URL || 'postgres://postgres:Skrillexfan7@localhost:5432';
 const client = new pg.Client(conString);
-pg.defaults.ssl = true;
+// pg.defaults.ssl = true;
 client.connect();
 client.on('error', err => console.error('you fucked up somehow man, here\'s the error ' + err));
 app.use(bodyParser.json());
