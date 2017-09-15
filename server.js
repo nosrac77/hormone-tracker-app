f3b40a2120df049cc6ae5bf1f864d33b7ec4ae12
nosrac77 <carson.newton@outlook.com>
2017-09-15 16:27:02 -0700

    attempting to send a response from query two since everything else is working fine now

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ app.post('/submit', function(request, response) {
         if (err) console.error(err)
         queryThree(result.rows[0].user_id);
         console.log(result.rows[0].user_id);
+        response.send('hello');
       }
     )
   }
