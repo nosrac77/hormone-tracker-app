ebe259d837511c300b41bb4e51d77b7ae52e9857
nosrac77 <carson.newton@outlook.com>
2017-09-15 16:39:14 -0700

    attempting to place result in new buffer

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.post('/submit', function(request, response) {
         if (err) console.error(err)
         queryThree(result.rows[0].user_id);
         console.log(result.rows[0].user_id);
-        response.send(new Buffer('whoop'));
+        response.send(new Buffer(result));
       }
     )
   }
