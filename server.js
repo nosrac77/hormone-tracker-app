3101abf1c59c6935b3fb50c0583826d5a120ec8b
nosrac77 <carson.newton@outlook.com>
2017-09-15 17:02:54 -0700

    the deed is done!

--- server.js
+++ server.js
@@ -30,8 +30,7 @@ app.post('/submit', function(request, response) {
         if (err) console.error(err)
         queryThree(result.rows[0].user_id);
         console.log(result.rows[0].user_id);
-        var id = result.rows[0].user_id;
-        response.send('' + id);
+        response.send('' + result.rows[0].user_id);
       }
     )
   }
